4833be291dc686f27c946dab01096a51ad96e68b
daniw <dani.winz@bluewin.ch>
2015-09-29 16:57:04 +0200

    corrected move.sh to move output files only

--- bldc/move.sh
+++ bldc/move.sh
@@ -6,4 +6,9 @@ do
     let i=i+1
 done
 mkdir test$i
-mv bldc_* test$i/
+mv bldc_*.ans test$i/
+mv bldc_*.bmp test$i/
+mv bldc_*.fem test$i/
+mv bldc_*.log test$i/
+mv bldc_*.mat test$i/
+mv bldc_*.pdf test$i/
